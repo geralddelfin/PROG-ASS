d25585c939592c9439f71f721f75f939413783c0
geralddelfin <42112978+geralddelfin@users.noreply.github.com>
2018-08-05 19:37:52 +0800

    Add files via upload

---
+++ GERALDDELFIN.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    cout<<"How to Log in to Facebook Account" << endl;
+    cout<<"Step 1. Go to www.fb.com" << endl;
+    cout<<"Step 2. Enter E-mail ID and password" << endl;
+    cout<<"Step 3. Click on log in button." << endl; 
+    return 0;
+}
